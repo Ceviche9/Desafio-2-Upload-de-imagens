d54647ce6268f8624bdceae0639bb5c963d90cfd
Tundê <ayotunde_sales@hotmail.com>
2022-01-12 12:24:30 -0300

    feat(index): add getImages funtion

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,40 @@ import { api } from '../services/api';
 import { Loading } from '../components/Loading';
 import { Error } from '../components/Error';
 
+type ImageResponseData = {
+  title: string;
+  description: string;
+  url: string;
+  ts: number;
+  id: number;
+};
+
+type GetImagesResponse = {
+  data: ImageResponseData[];
+  after: string | null;
+};
+
 export default function Home(): JSX.Element {
+  /*
+    1. Uma função que recebe como parâmetro um objeto que contêm a propriedade `pageParam`
+    (caso o parâmetro não exista, utilize como `default` o valor `null`). Esse parâmetro
+    é utilizado no momento da requisição para chamarmos uma próxima página.
+    Já no corpo da função, você deve realizar uma requisição GET para a rota `/api/images`
+    da API do Next.js informando como um `query param` de nome `after` o valor do `pageParam`
+    e retornar os dados recebidos.
+  */
+  const getImagesWithPagination = async ({
+    pageParam = null,
+  }): Promise<GetImagesResponse> => {
+    const response = await api.get('/api/images', {
+      params: {
+        after: pageParam,
+      },
+    });
+
+    return response.data;
+  };
+
   const {
     data,
     isLoading,
@@ -16,12 +49,9 @@ export default function Home(): JSX.Element {
     isFetchingNextPage,
     fetchNextPage,
     hasNextPage,
-  } = useInfiniteQuery(
-    'images',
-    // TODO AXIOS REQUEST WITH PARAM
-    ,
-    // TODO GET AND RETURN NEXT PAGE PARAM
-  );
+  } = useInfiniteQuery('images', getImagesWithPagination, {
+    getNextPageParam: lastPage => (lastPage.after ? lastPage.after : null),
+  });
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
